4a6165dd7bbab42bba48a5f932c980e79ba3bc92
Mike Combs <mike@combsnet.com>
2019-07-25 12:58:02 -0400

    feat: require login to post blogs

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
@@ -28,7 +29,7 @@ class PostDetailView(DetailView):
     ordering = ['-date_posted']
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
     # expects <model_form>
