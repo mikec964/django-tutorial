85ff2670ef4f1523c327f70eee007f37d0eaa3e4
Mike Combs <mike@combsnet.com>
2019-07-25 13:03:04 -0400

    feat: update blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .models import Post
 
 
@@ -30,6 +30,16 @@ class PostDetailView(DetailView):
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    # expects <model_form>
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
     # expects <model_form>
