69f9396d91091d0e451bdd0bbd5601e326b4871b
Mike Combs <mike@combsnet.com>
2019-07-25 21:25:15 -0400

    feat: filter posts by user

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (PostCreateView, PostDeleteView, PostDetailView, 
-                    PostListView, PostUpdateView)
+                    PostListView, PostUpdateView, UserPostListView)
 from . import views
 
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('post/new', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
 ]
